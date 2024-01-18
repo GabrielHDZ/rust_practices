4abc9ba0ee5047a40564765234278e6631b0b8f4
GabrielHDZ <gabrielhdz0102@gmail.com>
2024-01-17 23:40:28 -0600

    example rectangle

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,17 @@ fn main() {
         width:32.5,
         height:55.32
     };
+    let scale =2;
+    let rect2:Rectangle=Rectangle{
+        width:dbg!(3.3 * scale as f32),
+        height:12.3
+    };
     println!("Hello, world!, {:?}",calculo_rectangulo(&rectangulo1));
+    dbg!(&rect2);
 }
 struct Unit;
 struct Point(f32,f32);
+#[derive(Debug)]
 struct Rectangle{
     width:f32,
     height:f32,
