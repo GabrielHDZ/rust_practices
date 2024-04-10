9174d3aaba72354ba5ce44f57143b5a9eb002c9b
GabrielHDZ <gabrielhdz0102@gmail.com>
2024-04-09 21:35:02 -0600

    constants

--- src/session01.rs
+++ src/session01.rs
@@ -1,6 +1,9 @@
+/// The function `session01` in Rust defines constants and variables to demonstrate basic usage.
 fn session01(){
     const MAX_POINTS:u32=100_000;
     let mut counter=1;
     counter+=1;
-    
+    let counter=counter*2;
+    println!("max points {:?}",MAX_POINTS);
+    println!("counter value {:?}", counter);    
 }
