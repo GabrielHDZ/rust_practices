c17c0b57ae450542f3685c2261751adf25c6cb13
GabrielHDZ <gabrielhdz0102@gmail.com>
2024-04-12 23:18:11 -0600

    update

--- src/contratos.rs
+++ src/contratos.rs
@@ -1,4 +1,5 @@
 fn ejemplo(){
+    let months = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
     let contrato:(&str,i64,bool)=("carlos",2_500,false);
     let (name,amount,status)=contrato;
 }
