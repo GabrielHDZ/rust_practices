e9add6a4e5dbe0ef33942be1838cc55207ba47a5
GabrielHDZ <gabrielhdz0102@gmail.com>
2024-04-17 23:55:32 -0600

    c

--- src/contratos.rs
+++ src/contratos.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 pub fn ejemplo(){
-    
+
     //example array, solo guarda datos del mismo tipo
     let months = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
     //example tupla, puede guardar datos de diferentes tipos
@@ -18,6 +18,7 @@ pub fn ejemplo(){
     println!("{entrada}");
 
     //stack
+    
     let s3="mundo";
     let s4=s3;
     
