12620eb375d7110daa483d552db880ee1944645a
GabrielHDZ <gabrielhdz0102@gmail.com>
2024-04-19 01:37:26 -0600

    example

--- src/contratos.rs
+++ src/contratos.rs
@@ -1,24 +1,22 @@
 use std::io;
 
 pub fn ejemplo(){
-
     //example array, solo guarda datos del mismo tipo
     let months = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
     //example tupla, puede guardar datos de diferentes tipos
     let contrato:(&str,i64,bool)=("carlos",2_500,false);
     let (name,amount,status)=contrato;
-    //heap
+
+    //!declaramos una variable de tipo String, que se inicializa en el heap
     let mut entrada=String::new();
     println!("ingrese el mes de consulta de datos");
     io::stdin()
         .read_line(&mut entrada)
         .expect("Mes no registrado, para el usuario");       
-
     let s2=entrada.clone();
     println!("{entrada}");
 
     //stack
-    
     let s3="mundo";
     let s4=s3;
     
