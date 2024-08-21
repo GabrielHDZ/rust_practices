34696d27224939d1f1d73812995ec0540e047985
GabrielHDZ <gabrielhdz0102@gmail.com>
2024-08-20 23:19:30 -0600

    main update

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,17 @@ fn main(){
         let word = session02::first_word(&my_string[..]);
         // `first_word` también funciona con referencias de un string, que son equivalentes
         // a un slice completo de un String
-        let word = first_word(&my_string);
+        let word = session02::first_word(&my_string);
     
         let my_string_literal = "hello world";
     
         // `first_word` funciona con slices de string literales, sean parciales o completos
-        let word = first_word(&my_string_literal[0..6]);
-        let word = first_word(&my_string_literal[..]);
+        let word = session02::first_word(&my_string_literal[0..6]);
+        let word = session02::first_word(&my_string_literal[..]);
     
         // Por que los strings literales son slices de strings,esto también funciona,
         // sin necesidad de usar la sintaxis de slices.
-        let word = first_word(my_string_literal);
+        let word = session02::first_word(my_string_literal);
     }
     let mut para:String=String::from("Holas palabras");
     let ress=session02::first_word(&para);
